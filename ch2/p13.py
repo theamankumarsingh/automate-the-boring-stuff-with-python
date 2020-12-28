45c9c1de828ab3069517e5369f520b7aeb70bcbb
theamankumarsingh <amankumarsingh.professional@gmail.com>
2020-12-28 19:26:49 +0530

    Added solutions to chapter 2

---
+++ ch2/p13.py
@@ -0,0 +1,8 @@
+for i in range(10):
+    print(i+1)
+
+#part2
+i=0
+while i<10:
+    print(i+1)
+    i+=1
---
+++ ch2/p9.py
@@ -0,0 +1,7 @@
+spam=int(input())
+if spam==1:
+    print("Hello")
+elif spam ==2:
+    print("Howdy")
+else:
+    print("Greetings!")
